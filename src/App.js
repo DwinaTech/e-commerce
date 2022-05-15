3337608a2e865ee3469e2fb6ca309bc35264a116
Mohammed Dwina <diwina1993@hotmail.com>
2022-05-15 21:46:59 +0100

    Add React Routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import Chart from "./components/Chart";
+import { Jewelry } from "./components/Jewelry";
+import { MenClothes } from "./components/MenClothes";
+import { Electronics } from "./components/Electronics";
+import { WomenClothes } from "./components/WomenClothes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="chart" element={<Chart />} />
+        <Route path="jewelry" element={<Jewelry />} />
+        <Route path="electronics" element={<Electronics />} />
+        <Route path="men-clothing" element={<MenClothes />} />
+        <Route path="women-clothing" element={<WomenClothes />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
