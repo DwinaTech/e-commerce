e5d50db06c56f6424f641506213b209e4168b1de
Mohammed Dwina <diwina1993@hotmail.com>
2022-05-15 21:45:47 +0100

    Create <Home />

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import * as React from "react";
+import { Layout } from "../Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Typography component="h1">Home pgae</Typography>
+    </Layout>
+  );
+};
+
+export default Home;
