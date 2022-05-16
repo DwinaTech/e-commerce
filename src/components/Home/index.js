adf924a3970760d1dc441e9a60d0524bd19f83f5
Mohammed Dwina <diwina1993@hotmail.com>
2022-05-16 08:58:04 +0100

    Refactoring Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,16 +7,15 @@ import { useHome } from "./useHome";
 const Home = () => {
   const { products } = useHome();
 
+  if (!products.length) return;
   return (
     <Layout>
-      <Grid container spacing={2}>
-        {products.length
-          ? products.map((product) => (
-              <Grid item xs={6} sm={4} lg={3} key={product.id}>
-                <CustomCard product={product} />
-              </Grid>
-            ))
-          : null}
+      <Grid container spacing={{ xs: 2, sm: 3 }}>
+        {products.map((product) => (
+          <Grid item xs={6} sm={4} lg={3} key={product.id}>
+            <CustomCard product={product} />
+          </Grid>
+        ))}
       </Grid>
     </Layout>
   );
