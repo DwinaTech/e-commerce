e5975fe2980bef7673ba70e90b435b9379007467
Mohammed Dwina <diwina1993@hotmail.com>
2022-05-21 10:19:57 +0100

    Configure graphql and apollo client

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ import { MenClothes } from "./components/MenClothes";
 import { Electronics } from "./components/Electronics";
 import { WomenClothes } from "./components/WomenClothes";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SingleProduct } from "./components/SingleProduct";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="chart" element={<Chart />} />
-        <Route path="jewelry" element={<Jewelry />} />
-        <Route path="electronics" element={<Electronics />} />
-        <Route path="men-clothing" element={<MenClothes />} />
-        <Route path="women-clothing" element={<WomenClothes />} />
-      </Routes>
+      <ApolloProvider client={client}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="chart" element={<Chart />} />
+          <Route path="jewelry" element={<Jewelry />} />
+          <Route path="electronics" element={<Electronics />} />
+          <Route path="men-clothing" element={<MenClothes />} />
+          <Route path="women-clothing" element={<WomenClothes />} />
+          <Route path="single-product/:id" element={<SingleProduct />} />
+        </Routes>
+      </ApolloProvider>
     </BrowserRouter>
   );
 };
